849ca5733fb1fd69bbf988693bc3931fdea97b6a
WilliamWuisantono <w.justin.w@gmail.com>
2024-10-16 18:36:57 -0500

    Changes to Takehome  (Object to int)

--- Session7/PythonTakeHome-WilliamWuisantono.py
+++ Session7/PythonTakeHome-WilliamWuisantono.py
@@ -7,24 +7,24 @@ import matplotlib.pyplot as plt
 np.random.seed(42)
 
 # Define dataset size
-data_size = 20
+data_size = 50
 
 # Exercise 5: Generate a sample dataset and display a pairplot
 # Create a sample dataset with numpy np.random.randn(), np.random.rand(), np.random.randint()
 data = pd.DataFrame({
-    'Variable_A': [np.random.randn(data_size)],
-    'Variable_B': [np.random.randn(data_size)],
-    'Variable_C': [np.random.randint(1, 100, size=data_size)],
-    'Variable_D': [np.random.randn(data_size) * 10],
+    'Variable_A': np.random.randn(data_size),
+    'Variable_B': np.random.randn(data_size),
+    'Variable_C': np.random.randint(1, 100, size=data_size),
+    'Variable_D': np.random.randn(data_size) * 10,
 })
 
-# Create a pairplot of the dataset
+print(data.dtypes)
 
+# Create a pairplot of the dataset
 sns.pairplot(data)
 
 # Show the plot
-
 plt.show()
 
 # Optionally, save the plot as a PNG file
-plt.savefig('pairplot_output.png')
+# plt.savefig('pairplot_output.png')
